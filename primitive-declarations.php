b9e01cf2a888c45d28ec16abe035450d8f2b530c
Lollce <mathsou04@gmail.com>
2025-03-02 18:06:19 +0100

    ajoutt

---
+++ primitive-declarations.php
@@ -0,0 +1,14 @@
+<?php
+
+$firstStr = "Ceci est une chaîne de caractères.";
+$firstEscapeStr = "Ceci est une chaîne avec un retour à la ligne.\nNouvelle ligne ici.";
+$firstBool = true;
+$firstInteger = 42;
+$firstFloat = 3.14;
+$firstNull = null;
+
+$firstArray = [2, null, 'Hi', 5.5, false];
+
+print_r($firstArray); // Pour afficher le contenu du tableau
+
+?>
